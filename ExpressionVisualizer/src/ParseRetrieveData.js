5c7c7f6b5118dc0cf93ca461b881fc718b90ffea
Sid <siddardha.iiit@gmail.com>
2015-08-31 01:57:57 -0400

    using d3 graph to display visualizations based on survey selected. Adding some references that we can look use for enhancing d3 graphs

--- ExpressionVisualizer/src/ParseRetrieveData.js
+++ ExpressionVisualizer/src/ParseRetrieveData.js
@@ -4,70 +4,77 @@
 
 var Parse = require('parse').Parse;
 var requestModule = require("request");
-
+var Promise = require('promise');
 
 module.exports =
 {
-    initialize:function () {
+    initialize: function () {
         Parse.initialize("DYlc2hnyF3zlDZNcxhDE2zgSk87eQOdRCOJhgVSQ", "KdMQLJUBZ4vCTS78XTCVwCPpnlRvjnDWk8mef79l");
     },
 
-    GetUsers:function (){
+    GetUsers: function () {
         var surveyUsers = Parse.Object.extend("User");
         var query = new Parse.Query(surveyUsers);
         var rtn = [];
 
-        return query.find().then(function(res){
-            res.forEach(function(value , index , ar)  {
-                console.log(value.get("username"));
-                rtn.push({ username: value.get("username") , objectId: value.id});
+        return query.find().then(function (res) {
+                res.forEach(function (value, index, ar) {
+                    console.log(value.get("username"));
+                    rtn.push({username: value.get("username"), objectId: value.id});
+                });
+                return rtn;
+            },
+            function (fail) {
+                console.log("failed");
             });
-            return rtn;
-        } ,
-        function(fail){
-            console.log("failed");
-        });
     },
 
-    GetSurveys:function (userObjectID){
+    GetSurveys: function (userObjectID) {
         var User = Parse.Object.extend("User");
         var selecteduser = new User();
-        selecteduser .id = userObjectID;
+        selecteduser.id = userObjectID;
 
 
         var surveyedData = Parse.Object.extend("SurveyData");
         var query = new Parse.Query(surveyedData);
-        query.equalTo("UserID",selecteduser );
+        query.equalTo("UserID", selecteduser);
         var rtn = [];
 
-        return query.find().then(function(res){
-                res.forEach(function(value , index , ar)  {
+        return query.find().then(function (res) {
+                res.forEach(function (value, index, ar) {
                     console.log(value.createdAt);
-                    rtn.push({ createdat: value.createdAt , objectId: value.id});
+                    rtn.push({createdat: value.createdAt, objectId: value.id});
                 });
                 return rtn;
-            } ,
-            function(fail){
+            },
+            function (fail) {
                 console.log("failed");
             });
     },
 
 
-    GetSurveyId:function (surveyId){
+    GetSurveyId: function (surveyId) {
         var SurveyData = Parse.Object.extend("SurveyData");
         var query = new Parse.Query(SurveyData);
+        var rtn;
         return query.get(
-            surveyId).then(function(res) {
-                return requestModule({
-                        url: res.get("JsonEmotionData")._url,
-                        json: true,
-                    }, function (error, response, body) {
-                        if (!error && response.statusCode === 200) {
-                            console.log(body) // Print the json response
-                            return body;
+            surveyId).then(
+            function (res) {
+                return new Promise(function (resolve, reject) {
+                    requestModule({
+                            url: res.get("JsonEmotionData")._url,
+                            json: true,
+                        }, function (error, response, body) {
+                            if (!error && response.statusCode === 200) {
+                                console.log(body) // Print the json response
+                                rtn = body;
+                                resolve(rtn);
+                            } else {
+                                if (error) reject(error);
+                            }
                         }
-                    }
-                )
+                    )
+                })
             },
             function (error) {
                 console.log(error);
@@ -75,25 +82,25 @@ module.exports =
         );
 
 
-/*
-        var selecteduser = new User();
-        selecteduser .id = userObjectID;
+        /*
+         var selecteduser = new User();
+         selecteduser .id = userObjectID;
 
 
-        var surveyedData = Parse.Object.extend("SurveyData");
-        var query = new Parse.Query(surveyedData);
-        query.equalTo("UserID",selecteduser );
-        var rtn = [];
+         var surveyedData = Parse.Object.extend("SurveyData");
+         var query = new Parse.Query(surveyedData);
+         query.equalTo("UserID",selecteduser );
+         var rtn = [];
 
-        return query.find().then(function(res){
-                res.forEach(function(value , index , ar)  {
-                    console.log(value.createdAt);
-                    rtn.push({ createdat: value.createdAt , objectId: value.id});
-                });
-                return rtn;
-            } ,
-            function(fail){
-                console.log("failed");
-            });*/
+         return query.find().then(function(res){
+         res.forEach(function(value , index , ar)  {
+         console.log(value.createdAt);
+         rtn.push({ createdat: value.createdAt , objectId: value.id});
+         });
+         return rtn;
+         } ,
+         function(fail){
+         console.log("failed");
+         });*/
     }
 }
